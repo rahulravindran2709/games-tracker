c740424d1cb2f038d015bdb4e5970d25a2f50539
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-08-08 21:13:18 +0530

    Adding thunk and axios

--- src/ui/actions/index.js
+++ src/ui/actions/index.js
@@ -1,12 +1,18 @@
-import {LOGIN_STARTED, LOGIN_SUCCESS} from './types'
-export const startLogin = () => {
-  return {
-    type: LOGIN_STARTED
-  }
-}
-export const loginSucceeded = (data) => {
-  return {
+import { LOGIN_STARTED, LOGIN_SUCCESS, SEARCH_GAMES, GET_GAME_BY_ID } from './types';
+
+export const startLogin = () =>
+  ({
+    type: LOGIN_STARTED,
+  });
+
+export const loginSucceeded = data =>
+  ({
     type: LOGIN_SUCCESS,
-    payload: data
-  }
-}
+    payload: data,
+  });
+export const searchGames = () => ({
+  type: SEARCH_GAMES,
+});
+export const lookupGamesByName = searchTerm => dispatch => {
+
+};
--- src/ui/store/gametracker.js
+++ src/ui/store/gametracker.js
@@ -1,6 +1,7 @@
-import {createStore, applyMiddleware} from 'redux'
-import logger from 'redux-logger'
-import reducer from '../reducers'
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
 
-const store = createStore(reducer, applyMiddleware(logger))
+const store = createStore(reducer, applyMiddleware(logger, thunk))
 export default store
