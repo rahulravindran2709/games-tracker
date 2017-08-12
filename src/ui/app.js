acfad1d93f3d39547fb863a7d2c4f54e42993c2d
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-08-13 01:13:29 +0530

    Updated entry point

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import GamesTrackerApp from 'components/GamesApp';
 import store from './store/gametracker';
 
-injectTapEventPlugin();
-const App = () => (<MuiThemeProvider>
-  <Provider store={store}><GamesTrackerApp /></Provider></MuiThemeProvider>);
-ReactDOM.render(<Router ><App /></Router>, document.getElementById('app'));
+const App = () => (<Router ><MuiThemeProvider>
+  <Provider store={store}><GamesTrackerApp /></Provider></MuiThemeProvider></Router>);
+export default App;
