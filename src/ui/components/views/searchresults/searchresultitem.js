721c5b12900901e85cf859e1a4a12f318c1613f4
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-08-19 20:50:01 +0530

    Moment js added

--- src/ui/components/views/searchresults/searchresultitem.js
+++ src/ui/components/views/searchresults/searchresultitem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { GridListTileBar } from 'material-ui/GridList';
 import InfoIcon from 'material-ui-icons/Info';
 import IconButton from 'material-ui/IconButton';
@@ -11,7 +12,7 @@ const propTypes = {
 const defaultProps = {
   game: {},
 };
-const getImageCoverUrl = (url) => url.replace('t_thumb', 't_cover_big')
+const getImageCoverUrl = url => url.replace('t_thumb', 't_cover_big');
 const SearchResultItem = ({ game }) => (
   <div>
     <img src={getImageCoverUrl(game.cover.url)} alt={game.name} />
@@ -19,15 +20,15 @@ const SearchResultItem = ({ game }) => (
       title={game.name}
       subtitle={
         <span>
-         Released: {game.first_release_date}
-       </span>
+         Released: {moment(game.first_release_date).format('DD-MM-YYYY')}
+        </span>
      }
       actionIcon={
         <IconButton>
           <InfoIcon color="rgba(255, 255, 255, 0.54)" />
         </IconButton>
      }
-   />
+    />
   </div>
 );
 
