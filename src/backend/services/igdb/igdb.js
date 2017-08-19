4e2d9f8db4c8f36ebaacb4d6d151f786f1c3a451
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-08-19 13:13:34 +0530

    Setting up search options

--- src/backend/services/igdb/igdb.js
+++ src/backend/services/igdb/igdb.js
@@ -5,14 +5,28 @@ const serviceMethods = {};
 
 global['3scaleKey'] = '422b0b250799d114e611860b340af41d';
 const client = igdb();
-serviceMethods.getGames = (term, zone ) => {
+const createSearchOptions = (options) => {
+  const defaultSearchOptions = {
+    fields: '*',
+    limit: 10,
+    offset: 0,
+  };
+  return { ...defaultSearchOptions, ...options.search, };
+}
+serviceMethods.getGames = (term, zone) => {
   console.log('In search games');
   const data = require(require('path').resolve(process.cwd(), 'data/search.json'));
-  return new Promise((resolve, reject) =>resolve(data))
+  const searchOptions = createSearchOptions({
+    search: term,
+  });
+  console.log(searchOptions, 'Final search options');
+  const filteredData = data.filter((game) => game.name.toLowerCase().indexOf(searchOptions.term.toLowerCase()) !== -1)
+  return new Promise((resolve, reject) =>resolve(filteredData))
   /*return client.games({
     fields: '*',
     limit: 10,
     offset: 15,
+    search: term,
   }).then(response => response.body)
   .catch((error) => {
     console.log(error, 'Error occurred');
