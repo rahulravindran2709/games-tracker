2b28b1a69e3a826709e7eebddc339dd996f404de
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-09-09 15:31:41 +0530

    Rating widget

---
+++ src/ui/components/widgets/rating/rating.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { times } from 'ramda';
+import StarRateIcon from 'material-ui-icons/Star';
+
+
+const propTypes = {
+  value: PropTypes.number.isRequired,
+};
+const renderRatingStar = () => (<StarRateIcon style={{ color: 'aqua' }} />);
+const Rating = ({ value = 0 }) => (<div>{times(renderRatingStar)(value)}</div>);
+
+Rating.propTypes = propTypes;
+export default Rating;
