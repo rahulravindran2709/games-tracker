3476857dba62e31bebed73facd2b2045fff0a9c2
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-09-09 12:51:13 +0530

    Banner v1

--- src/ui/components/views/gamedetails/banner/gamedetailsbanner.js
+++ src/ui/components/views/gamedetails/banner/gamedetailsbanner.js
@@ -1,74 +1,78 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import style from './style.scss';
 
-const GameDetailsBanner = props => (<div className="game-details__banner">
-  <div className="game-details__banner__header">
-    <div className="mui-container">
-      <div className="mui-row">
-        <div className="mui-col-md-6">
-          <h1>Storm blade</h1>
+const styles = () => ({
+  root: {
+    flexGrow: 1,
+  },
+});
+const GameDetailsBanner = ({ classes }) => (<div className="banner">
+  <div className="header">
+    <Grid container className={classes.root}>
+      <Grid item md={6}>
+        <Typography type="display2" gutterBottom className="shadow text">Storm blade</Typography>
+      </Grid>
+      <Grid item md={1}>
+        <div className="ageRatings">
+          <h1>
+            <a title="By PEGI, extrahiert von StG1990 [Public domain], via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File%3APEGI_18.svg">
+              <img width={16} alt="PEGI 18" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/PEGI_18.svg/16px-PEGI_18.svg.png" />
+            </a>
+            <a title="By Entertainment Software Association (Personal correspondence) [Public domain], via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File%3AESRB_2013_Rating_Pending.svg">
+              <img width={16} alt="ESRB 2013 Rating Pending" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/ESRB_2013_Rating_Pending.svg/16px-ESRB_2013_Rating_Pending.svg.png" /></a>
+          </h1>
         </div>
-        <div className="game-details__ratings">
-          <div className="mui-col-md-1">
-            <h1>
-              <a title="By PEGI, extrahiert von StG1990 [Public domain], via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File%3APEGI_18.svg">
-                <img width={16} alt="PEGI 18" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/PEGI_18.svg/16px-PEGI_18.svg.png" />
-              </a>
-              <a title="By Entertainment Software Association (Personal correspondence) [Public domain], via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File%3AESRB_2013_Rating_Pending.svg">
-                <img width={16} alt="ESRB 2013 Rating Pending" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/ESRB_2013_Rating_Pending.svg/16px-ESRB_2013_Rating_Pending.svg.png" /></a>
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   </div>
-  <div className="game-details__meta">
-    <div className="mui-container-fluid">
-      <div className="mui-row">
-        <div className="mui-col-md-3" />
-        <div className="mui-col-md-3">
-          <div className="game-details__meta__user-rating">
-            <span className="stars">
-              <i className="fa fa-star" aria-hidden="true" />
-              <i className="fa fa-star" aria-hidden="true" />
-              <i className="fa fa-star" aria-hidden="true" />
-              <i className="fa fa-star" aria-hidden="true" />
-              <i className="fa fa-star" aria-hidden="true" />
-            </span>
-            <span className="ratings-text">
-                      4.1/5 <span className="reviews-number">(33 reviews)</span>
-            </span>
-          </div>
+  <div className="meta">
+    <Grid container className={classes.root}>
+      <Grid item md={3} />
+      <Grid item md={3}>
+        <div className="user-rating">
+          <span className="stars">
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+            <i className="fa fa-star" aria-hidden="true" />
+          </span>
+          <Typography type="subheading" className="shadow text"><span className="ratings-text">
+                          4.1/5 <span className="reviews-number">(33 reviews)</span>
+          </span></Typography>
+
         </div>
-        <div className="mui-col-md-3">
-          <div className="game-details__meta__release-date">
-            <span>Released 19th July, 2017</span>
-          </div>
+      </Grid>
+      <Grid item md={3}>
+        <div className="release-date">
+          <Typography type="subheading" className="shadow text">Released 19th July, 2017</Typography>
         </div>
-      </div>
-      <div className="game-details__meta__user-meta">
-        <div className="mui-row">
-          <div className="mui-col-md-3">
-            <div className="game-details__meta__user-meta__last-played">
-              <h3>Last played 27th January, 2017</h3>
-            </div>
-          </div>
-          <div className="mui-col-md-3">
-            <div className="game-details__meta__user-meta__hours_played">
-              <h3>15 hours spent</h3>
-            </div>
-          </div>
-          <div className="mui-col-md-3">
-            <div className="game-details__meta__user-meta__playthroughs">
-              <h3>1 completed playthrough</h3>
-            </div>
-          </div>
+      </Grid>
+    </Grid>
+    <Grid container className={classes.root}>
+      <Grid item md={3}>
+        <div className="last-played">
+          <Typography type="subheading" className="shadow text">Last played 27th January, 2017</Typography>
         </div>
-      </div>
-    </div>
+      </Grid>
+      <Grid item md={3}>
+        <div className="hours_played">
+          <Typography type="subheading" className="shadow text">15 hours spent</Typography>
+        </div>
+      </Grid>
+      <Grid item md={3}>
+        <div className="playthroughs">
+          <Typography type="subheading" className="shadow text">1 completed playthrough</Typography>
+        </div>
+      </Grid>
+    </Grid>
   </div>
   <div className="game-details__actions">
     <button className="mui-btn mui-btn--fab mui-btn--primary">+</button>
   </div>
 </div>);
-
-export default GameDetailsBanner;
+const stylesHOC = withStyles(styles);
+export default stylesHOC(GameDetailsBanner);
