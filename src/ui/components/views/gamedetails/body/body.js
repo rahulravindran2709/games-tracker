58706ac486ab9b288abda869f4e9591d3020029f
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-09-09 21:02:02 +0530

    Refactoring into files

--- src/ui/components/views/gamedetails/body/body.js
+++ src/ui/components/views/gamedetails/body/body.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
-import Chip from 'material-ui/Chip';
-import Avatar from 'material-ui/Avatar';
-import deepPurple from 'material-ui/colors/deepPurple';
 import { withStyles } from 'material-ui/styles';
 import style from './style.scss';
-
+import GenreSection from './genre';
+import DevPubGrid from './devpub';
 
 const styles = theme => ({
   root: {
@@ -18,81 +17,26 @@ const styles = theme => ({
   meta: {
     height: '100px',
   },
-  avatar: {
-    margin: 0,
-    color: '#fff',
-    backgroundColor: deepPurple[500],
-  },
 });
-const DevPublGrid = ({ classes }) => (<Grid container className={classes.meta} justify={'center'} align={'center'}>
-  <Grid item md={3}>
-    <Grid container justify={'center'} align={'center'}>
-      <Grid item md={3}>
-        <Typography type="button">Publisher</Typography>
-      </Grid>
-      <Grid item md={6}>
-        <Typography type="button">
-          <Chip
-            avatar={<Avatar className={classes.avatar}>U</Avatar>}
-            label="Ubisoft"
-            className={classes.chip}
-          /></Typography>
-      </Grid>
-    </Grid>
-  </Grid>
-  <Grid item md={3}>
-    <Grid container justify={'center'} align={'center'}>
-      <Grid item md={3}>
-        <Typography type="button">Developer</Typography>
-      </Grid>
-      <Grid item md={6}>
-        <Typography type="button">
-          <Chip
-            avatar={<Avatar className={classes.avatar}>U</Avatar>}
-            label="Ubisoft"
-            className={classes.chip}
-          /></Typography>
-      </Grid>
-    </Grid>
-  </Grid>
-</Grid>);
-const GenreSection = ({ classes }) => (<Grid container className={classes.meta} justify={'center'} align={'center'}>
-  <Grid item md={5}>
-    <Grid container justify={'center'} align={'center'}>
-      <Grid item md={3}>
-        <Typography type="button">Genres</Typography>
-      </Grid>
-      <Grid item md>
-        <Typography type="button">
-          <Grid container align={'flex-start'}>
-            <Grid item>
-              <Chip
-                avatar={<Avatar className={classes.avatar}>A</Avatar>}
-                label="Action"
-                className={classes.chip}
-              /></Grid><Grid item><Chip
-                avatar={<Avatar className={classes.avatar}>A</Avatar>}
-                label="Adventure"
-                className={classes.chip}
-              /></Grid>
-            <Grid item>
-              <Chip
-                avatar={<Avatar className={classes.avatar}>A</Avatar>}
-                label="Action"
-                className={classes.chip}
-              /></Grid></Grid>
-        </Typography>
-      </Grid>
-    </Grid>
-  </Grid>
-</Grid>);
+
+const SummarySection = ({ description }) => (
+  <Grid container>
+    <Grid item md={6}><Typography type="headline">Summary</Typography>
+      <Typography gutterBottom noWrap>
+        {`
+    ${description}
+  `}
+      </Typography></Grid></Grid>);
 const GameDetailsBody = ({ classes }) => (
-  <Grid container className={classes.root} >
-    <Grid item md={12}>
-      <DevPublGrid classes={classes} />
-      <GenreSection classes={classes} />
+  <Grid container className={classes.root} justify={'center'}>
+    <Grid item md={6}>
+      <DevPubGrid classes={classes} />
+      <GenreSection classes={classes} genres={['Action', 'Adventure']} />
+      <SummarySection description={'Some short shitty summary'} />
     </Grid>
   </Grid>);
-
+GameDetailsBody.propTypes = {
+  classes: PropTypes.shape().isRequired,
+};
 const stylesHOC = withStyles(styles);
 export default stylesHOC(GameDetailsBody);
---
+++ src/ui/components/views/gamedetails/body/genre.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
+import deepPurple from 'material-ui/colors/deepPurple';
+import { map } from 'ramda';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  chip: {
+    margin: theme.spacing.unit,
+  },
+  meta: {
+    height: '100px',
+  },
+  avatar: {
+    margin: 0,
+    color: '#fff',
+    backgroundColor: deepPurple[500],
+  },
+});
+
+const GenreSection = ({ classes, genres }) => {
+  const genreList = map(genre => (<Grid item>
+    <Chip
+      avatar={<Avatar className={classes.avatar}>A</Avatar>}
+      label={genre}
+      className={classes.chip}
+    /></Grid>))(genres);
+  return (<Grid container className={classes.meta}>
+    <Grid item md={12}>
+      <Grid container align={'center'}>
+        <Grid item md={2}>
+          <Typography type="button">Genres</Typography>
+        </Grid>
+        <Grid item md>
+          <Typography type="button">
+            <Grid container align={'flex-start'}>
+              {genreList}
+            </Grid>
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  </Grid>);
+};
+GenreSection.propTypes = {
+  genres: PropTypes.arrayOf(PropTypes.string),
+  classes: PropTypes.shape().isRequired,
+};
+
+GenreSection.defaultProps = {
+  genres: [],
+};
+const stylesHOC = withStyles(styles);
+export default stylesHOC(GenreSection);
