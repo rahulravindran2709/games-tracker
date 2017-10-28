e1489a2a2d8a9c120218214ecaaa437e334d097a
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-10-28 16:39:47 +0530

    Console

--- src/backend/services/enums/enums.js
+++ src/backend/services/enums/enums.js
@@ -1,7 +1,7 @@
 
 export function getGenreById(id) {
-    console.log(this.model[0].create, 'this inside getgenrebyid')
-    return this.model[0].findOne({ where: { genre_id: id } })
-};
+  console.log('this inside getgenrebyid');
+  return this.model[0].findOne({ where: { genre_id: id } });
+}
 export const getPegiRatingById = (id) => {};
 export const getEsrbRatingById = (id) => {};
