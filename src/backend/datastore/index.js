0fb486888cc0a24a91091da47dccac8989189f71
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-10-28 11:42:39 +0530

    Refactoring to ramda

--- src/backend/datastore/index.js
+++ src/backend/datastore/index.js
@@ -1,9 +1,12 @@
 import Sequelize from 'sequelize';
+import { map, compose } from 'ramda';
 import Models from '../models';
+
 
 const sequelize = new Sequelize('gametracker_enum', 'gametracker', 't3rr1ble', {
   host: 'localhost',
   dialect: 'postgres',
+  operatorsAliases: Sequelize.Op,
   pool: {
     max: 5,
     min: 0,
@@ -11,18 +14,21 @@ const sequelize = new Sequelize('gametracker_enum', 'gametracker', 't3rr1ble', {
   },
 });
 
-const defineModelInSequelize = modelObj => sequelize.define(modelObj.name, modelObj.model)
+const defineModelInSequelize = modelObj => sequelize.define(modelObj.name, modelObj.model);
+const mapModelSyncFn = map(definedModel => definedModel.sync());
+const mapDefineModels = map(defineModelInSequelize);
 const register = (server, options, next) => {
   server.log(['plugin', 'info'], "Registering the 'datastore' plugin");
   sequelize
     .authenticate()
     .then(() => {
       server.log(['plugin', 'datastore'], 'Connection has been established successfully.');
-      const definedModels = Models.map(defineModelInSequelize)
-      .map(definedModel => definedModel.sync());
-      Promise.all(definedModels)
-      .then(md => console.log(md, 'Finished'))
-      server.expose('databaseModels', definedModels);
+      const definedModels = compose(mapModelSyncFn, mapDefineModels)(Models);
+      return Promise.all(definedModels)
+      .then((syncedModels) => {
+        console.log(syncedModels, 'Finished');
+        server.expose('DatabaseModels', syncedModels);
+      });
     })
     .catch((err) => {
       server.log('Unable to connect to the database:', err);
