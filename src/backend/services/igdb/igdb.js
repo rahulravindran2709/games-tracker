5c9823345081dcb66d9d7cbf7b33aec8eb4fac4b
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-12-06 11:46:37 +0530

    Working insert and subsequent select based on igdb id

--- src/backend/services/igdb/igdb.js
+++ src/backend/services/igdb/igdb.js
@@ -1,4 +1,6 @@
 import igdb from 'igdb-api-node';
+import { head, compose, pick, mergeAll, prop, objOf } from 'ramda';
+import { getBodyFromServiceResponse } from '../shared/utils';
 
 global['3scaleKey'] = '422b0b250799d114e611860b340af41d';
 const client = igdb();
@@ -27,19 +29,39 @@ export const getGames = (searchCriteria) => {
     throw error;
   });
 };
-export const getGameById = (id) => {
-  console.log(id, 'In get game by id');
+export function getGameById(igdbGameId) {
+  // TODO Add db call to check whether the game id exists
+  console.log(igdbGameId, 'In get game by id');
+  const { Game } = this.models;
+
   return client.games({
-    ids: [id],
+    ids: [igdbGameId],
   }).then((response) => {
-    console.log(response.body);
-    return response.body;
+    const gameObject = compose(head, getBodyFromServiceResponse)(response);
+    const pickRelevantFields = pick(['id', 'name',
+      'created_at', 'updated_at', 'summary', 'first_release_date']);
+    const defaultObject = pickRelevantFields(gameObject);
+    return Game.findCreateFind({
+      where: {
+        game_id: igdbGameId,
+      },
+      defaults: {
+        name: defaultObject.name,
+        game_createdAt: defaultObject.created_at,
+        game_updatedAt: defaultObject.updated_at,
+      },
+    })
+    .then(([modelObject, wasInserted]) => {
+      const igdbSubset = compose(objOf('igdbGameId'), prop('id'))(gameObject);
+      const dbSubset = pick(['id'])(modelObject);
+      return mergeAll([gameObject, dbSubset, igdbSubset]);
+    });
   })
   .catch((error) => {
     console.log(error, 'Error occurred');
     throw error;
   });
-};
+}
 
 export const getGenreById = (id) => {
   console.log(id, 'In get genre by id');
@@ -47,9 +69,9 @@ export const getGenreById = (id) => {
   }, [
     'name', 'id'],
 ).then((response) => {
-    console.log(response.body, 'Genre object received from igdb');
-    return response.body;
-  })
+  console.log(response.body, 'Genre object received from igdb');
+  return response.body;
+})
     .catch((error) => {
       console.log(error, 'Error occurred');
       throw error;
