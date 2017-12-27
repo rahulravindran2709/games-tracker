26a39c9f1d44396673ee0f191303e5ed2469b3f3
Rahul Ravindran <rahulravindran2709@gmail.com>
2017-12-27 08:10:15 +0530

    Code climate refactoring

--- src/backend/server/api.js
+++ src/backend/server/api.js
@@ -12,29 +12,34 @@ const validate = (decoded, request, callback) => {
   }
   return callback(null, true);
 };
+const initialiseJwtAuth = (server, configurationObject) => () => {
+  server.app.sessions = {};
+  const secret = configurationObject.get('apiServer:auth:secret');
+  server.auth.strategy('jwt', 'jwt', true, {
+    key: secret,
+    validateFunc: validate,
+    verifyOptions: { ignoreExpiration: true, algorithms: ['HS256'] },
+  });
+};
+const initialiseApiRoutes = (server, routeObject) => () => {
+  server.route(routeObject.api);
+  server.route(routeObject.enumerated);
+  server.route(routeObject.user);
+  server.route(routeObject.collection);
+};
 function register(server, options, next) {
   server.log(['plugin', 'info', 'api'], 'Registering the api server plugin');
   const configurationObject = getConfiguration(server);
   const corsOptions = configurationObject.get('apiServer:cors');
   server.register([{ register: cors, options: corsOptions }])
-  .then(() => server.register([{ register: authJwt }]))
-  .then(() => {
-    server.app.sessions = {};
-    const secret = configurationObject.get('apiServer:auth:secret');
-    server.auth.strategy('jwt', 'jwt', true, {
-      key: secret,
-      validateFunc: validate,
-      verifyOptions: { ignoreExpiration: true, algorithms: ['HS256'] },
-    });
-  })
-  .then(() => server.register([{ register: datastore },
+  .then(() => server.register([
+    { register: authJwt },
+  ]))
+  .then(initialiseJwtAuth(server, configurationObject))
+  .then(() => server.register([
+    { register: datastore },
     { register: serviceRegistry }]))
-  .then(() => {
-    server.route(routes.api);
-    server.route(routes.enumerated);
-    server.route(routes.user);
-    server.route(routes.collection);
-  })
+  .then(initialiseApiRoutes(server, routes))
   .then(() => {
     server.log(['plugin', 'info', 'api', 'routes'], 'Successfully configured routes');
     return next();
