ead3c27dab6b54592b48140b8370f082062d6c25
Rahul Ravindran <rahulravindran2709@gmail.com>
2018-01-04 10:40:09 +0530

    Removing redundant error messages

--- src/ui/constants/auth/urls/index.js
+++ src/ui/constants/auth/urls/index.js
@@ -1,3 +1,3 @@
 export const AUTHENTICATE = '/authenticate';
 export const LOGOUT = '/logout';
-export const REGISTER = '/users';
+export const REGISTER = '/users/';
--- src/ui/reducers/auth.js
+++ src/ui/reducers/auth.js
@@ -1,27 +1,17 @@
 import { AUTHENTICATE_FULFILLED,
-  AUTHENTICATE_REJECTED,
    LOGOUT_FULFILLED,
    LOGOUT_REJECTED } from 'constants/auth/actions';
-import { getActionType, getPayloadData, getErrorMessage } from './shared/utils';
+import { getActionType } from './shared/utils';
 
 const initialState = {
   token: null,
   username: null,
-  errors: [],
 };
 const auth = (state = initialState, action) => {
   const type = getActionType(action);
-  const data = getPayloadData(action);
   switch (type) {
     case AUTHENTICATE_FULFILLED:
       return { ...state, token: action.payload.headers.authorization };
-    case AUTHENTICATE_REJECTED: {
-      const errorMessage = getErrorMessage(action);
-      return { ...state,
-        errors: state.errors.concat({
-          errorMessage,
-        }) };
-    }
     case LOGOUT_FULFILLED:
     case LOGOUT_REJECTED:
       return { ...state, token: null, username: null };
